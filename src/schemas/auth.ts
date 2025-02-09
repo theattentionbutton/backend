4a44f41efc586e33e501c15ff1b3387d4648b8c3
Siddharth Singh <me@shantaram.xyz>
2025-02-09 09:28:36 +0530

    create updatePwSchema

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,5 +1,18 @@
 import { z } from "zod";
 
+export const updatePwSchema = z.object({
+    "old-pw": z.string().min(6).max(72),
+    "new-pw": z.string().min(6).max(72),
+    "confirm-new-pw": z.string().min(6).max(72),
+}).transform(data => ({
+    oldPass: data['old-pw'],
+    newPass: data['new-pw'],
+    confirmPw: data['confirm-new-pw']
+})).refine(data => data.newPass === data.confirmPw, {
+    message: "Passwords must match",
+    path: ["authConfirmPw"],
+});
+
 const baseLoginSchema = z.object({
     "auth-email": z.string().email(),
     "auth-password": z.string().min(6).max(72),
