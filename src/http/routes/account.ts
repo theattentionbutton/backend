451a4384182935cb29cf81c2afa67131dbd2daeb
Siddharth Singh <me@shantaram.xyz>
2025-02-10 09:15:30 +0530

    dashboard rendering works!

--- src/http/routes/account.ts
+++ src/http/routes/account.ts
@@ -1,7 +1,34 @@
 import express from "express";
+import "../../utils/express-session.d.ts";
+import { getRoomsById, getUserInvites, getUserRooms } from "../../db/rooms.ts";
+import { updatePwSchema } from "../../schemas/auth.ts";
+import { fromError } from "zod-validation-error";
+import { renderError } from "../../utils/index.ts";
+import { getUser, updatePassword } from "../../db/auth.ts";
+import { verify } from "argon2";
 
-export const get: express.Handler = (req, res, next) => {
+export const get: express.Handler = async (req, res, next) => {
+    const user = await getUser(req.session.username);
+    const userRooms = await getUserRooms(user.id);
+    const invites = await getUserInvites(user.id);
+    const invitedRooms = invites.map(i => i.room_id);
+    const rooms = await getRoomsById(userRooms.map(itm => itm.id).concat(invitedRooms));
+    const roomMap = Object.groupBy(rooms, (itm) => itm.room_id);
+
+    console.log({ userRooms, rooms });
+
     return res.render('dashboard', {
+        title: "Dashboard",
+        username: req.session.username,
+        rooms: rooms.filter(room => !invitedRooms.includes(room.room_id)),
+        invites: invites.map(invite => ({
+            id: invite.id,
+            from: invite.from,
+            roomName: roomMap[invite.room_id]
+        }))
+    });
+}
+
 export const updatePw: express.Handler = async (req, res, next) => {
     const parseResult = await updatePwSchema.safeParseAsync(req.body);
     if (!parseResult.success) {
